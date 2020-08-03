5a682010d9604bd342c33b43019e4ac03506fe4d
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-08-03 16:09:32 -0300

    feat: added Basic controller to create an event to create a book

--- library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -1,19 +1,35 @@
 package com.learnkafka.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.domain.LibraryEventType;
+import com.learnkafka.domain.producer.LibraryEventProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.concurrent.ExecutionException;
+
 @RestController
+@Slf4j
 public class LibraryEventsController {
 
-    @PostMapping("/v1/library-event")
-    public ResponseEntity<LibraryEvent> createLibraryEvent(@RequestBody LibraryEvent event) {
-        // invoke kafka producer
+    @Autowired
+    LibraryEventProducer libraryEventProducer;
 
+    @PostMapping("/v1/library-event")
+    public ResponseEntity<LibraryEvent> createLibraryEvent(@RequestBody @Valid LibraryEvent event) throws JsonProcessingException, ExecutionException, InterruptedException {
+        // invoke kafka producer
+        log.info("before sendLibraryEvent");
+        event.setType(LibraryEventType.NEW);
+        libraryEventProducer.sendlibraryEvent_Approach2(event);
+        log.info("after sendLibraryEvent");
         return ResponseEntity.status(HttpStatus.CREATED).body(event);
     }
 }
--- library-events-producer/src/main/java/com/learnkafka/domain/LibraryEvent.java
+++ library-events-producer/src/main/java/com/learnkafka/domain/LibraryEvent.java
@@ -5,11 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class LibraryEvent {
     private Integer libraryEventId;
+    private LibraryEventType type;
+    @NotNull
+    @Valid
     private Book book;
 }
